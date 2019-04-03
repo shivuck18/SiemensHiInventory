aaaea6fbbf8067fbb9c8927166dee09d00d80c21
Khanapur, Shivakumar (CT RDA DS AA TEC AU UI) <Shivakumar.Khanapur@siemens.com>
2019-04-03 16:38:27 +0530

    Project infrastructure

---
+++ Source/HIInventory/Inventory.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Inventory.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Source/HIInventory/Inventory.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Inventory.Repository
+{
+    public class Class1
+    {
+    }
+}
