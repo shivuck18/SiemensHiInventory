61d56729295bcf4499687c0160a6b644429fbd2e
saumya72 <saumya@siemens.com>
2019-04-04 09:47:10 +0530

    Added Platform Class.

---
+++ Source/HIInventory/Inventory.Domain/Platform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Inventory.Domain
+{
+    class Platform
+    {
+        [Key]
+        public int Id { get; set; }
+        public string PlatformName { get; set; }
+        public string PlatformType { get; set; }
+
+        public Platform platform { get; set; }
+    }
+}
