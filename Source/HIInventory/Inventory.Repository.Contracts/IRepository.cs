b0a95d894f1e68ee22c28781bd98d50614bf20ca
Gaurav Kumar <kumargaurav@siemens.com>
2019-04-08 10:58:39 +0530

    added generic repository

---
+++ Source/HIInventory/Inventory.Repository.Contracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory.Repository.Contracts
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        IList<T> GetAll();
+
+        T GetById(int id);
+    }
+}
---
+++ Source/HIInventory/Inventory.Repository/Repository.cs
@@ -0,0 +1,49 @@
+﻿using Inventory.Data;
+using Inventory.Repository.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inventory.Repository
+{
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public Repository(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public void Add(T entity)
+        {
+            _databaseContext.Set<T>().Add(entity);
+            _databaseContext.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _databaseContext.Set<T>().Remove(entity);
+            _databaseContext.SaveChanges();
+        }
+
+        public IList<T> GetAll()
+        {
+            return _databaseContext.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _databaseContext.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            _databaseContext.Entry(entity).State = EntityState.Modified;
+            _databaseContext.SaveChanges();
+        }
+    }
+}
