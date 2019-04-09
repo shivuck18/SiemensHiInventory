2493caee4ba35e48ad0b168709293449a79ace65
Jha, Anuradha (CT RDA DS AA TEC AU UI) <anuradha.jha@siemens.com>
2019-04-09 16:40:55 +0530

    UI Code changes for Platform page

--- Source/HIInventory/Inventory.Dto/PlatformDto.cs
+++ Source/HIInventory/Inventory.Dto/PlatformDto.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Inventory.Dto
 {
     public class PlatformDto
     {
-        [Key]
         public int Id { get; set; }
         public string PlatformName { get; set; }
-        public string ICType { get; set; }
+        public string IcType { get; set; }
     }
 }
--- Source/HIInventory/Inventory.UI/Controllers/PlatformController.cs
+++ Source/HIInventory/Inventory.UI/Controllers/PlatformController.cs
@@ -1,18 +1,93 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Inventory.Data;
+using Inventory.Dto;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Inventory.UI.Controllers
 {
     public class PlatformController : Controller
     {
+
+        //private readonly IPlatformService _platformService;
+
+        // created object of platformService 
+
+        //public PlatformController(IPlatformService platformService)
+        //{
+        //    _platformService = platformService;
+        //}
+
+
+        List<PlatformDto> platforms = new List<PlatformDto>();
+
         public IActionResult Index()
         {
+            //return View(_platformService.GettAll());
 
-            ViewData["Message"] = "This is Platform launch page.";
+            PopulateData();
+            return View(platforms);
+        }
+
+        public IActionResult Add()
+        {
+
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Add(PlatformDto PlatformDto)
+        {
+            //databaseContext.HiInventory.Add(PlatformDto);
+            //databaseContext.SaveChanges();
+
+            // IPlatformService.Add(platofrmDto); // TODO Call Business Service
+
+            //_platformService.add(PlatformDto);
+            platforms.Add(PlatformDto);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            //it will call _platformService interface method and edit with respect to id
+            // _platformService.edit(id);
+            PopulateData();
+            var platform = platforms.FirstOrDefault(x => x.Id == id);
+            return View("Add", platform);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            //it will call _platformService interface method and delete with respect to id
+            //_platformService.delete(id);
+            //below line will redirect to main page
+            return RedirectToAction(nameof(Index));
+        }
+
+        private void PopulateData()
+        {
+            platforms.Add(new PlatformDto()
+            {
+                Id = 1,
+                PlatformName = "Test Platform",
+                IcType = "Test Ic Type"
+            });
+
+            platforms.Add(new PlatformDto()
+            {
+                Id = 2,
+                PlatformName = "Test Platform 2",
+                IcType = "Test Ic Type 2"
+            });
+
+            platforms.Add(new PlatformDto()
+            {
+                Id = 3,
+                PlatformName = "Test Platform 3",
+                IcType = "Test Ic Type 3"
+            });
+        }
+
     }
 }
