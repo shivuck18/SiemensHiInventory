171c45df2b821f415489b949ebe049f02f122340
Gaurav Kumar <kumargaurav@siemens.com>
2019-04-11 16:28:00 +0530

    Update UI Validation

--- Source/HIInventory/Inventory.Dto/PlatformDto.cs
+++ Source/HIInventory/Inventory.Dto/PlatformDto.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Inventory.Dto
 {
     public class PlatformDto
     {
+        [Key]
+        [BindProperty, Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Platform Name cannot be blank.") ,MaxLength(6, ErrorMessage ="Platform Name cannot be more than 6 characters"), RegularExpression(@"^[a-zA-Z0-9+""'\s-]*$", ErrorMessage ="Platform name cannot have special characters other than '+'")]
         public string PlatformName { get; set; }
+        [Required(ErrorMessage = "ICType cannot be blank") ,MaxLength(2, ErrorMessage = "ICType cannot be more than 2 digts"), Range(1,999, ErrorMessage ="IC Type should be numerical")]
         public string ICType { get; set; }
     }
 }
