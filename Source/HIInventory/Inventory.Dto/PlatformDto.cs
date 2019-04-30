f420a2a96ac5c28f10f9a963839f1113f7130e24
saumya72 <saumya@siemens.com>
2019-04-30 15:47:20 +0530

    Renamed ICType to IC Type and PlatformName to Platform Name in all the actions- Add, Edit, and Layout and all other places as well.

--- Source/HIInventory/Inventory.Dto/PlatformDto.cs
+++ Source/HIInventory/Inventory.Dto/PlatformDto.cs
@@ -13,12 +13,12 @@ namespace Inventory.Dto
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Platform Name cannot be blank.") ,MaxLength(6, ErrorMessage ="Platform Name cannot be more than 6 characters"), RegularExpression(@"^[a-zA-Z0-9+""'\s-]*$", ErrorMessage ="Platform name cannot have special characters other than '+'")]
+        [Required(ErrorMessage = "Platform Name cannot be blank.") ,MaxLength(6, ErrorMessage ="Platform Name cannot be more than 6 characters"), Display(Name = "Platform Name"), RegularExpression(@"^[a-zA-Z0-9+""'\s-]*$", ErrorMessage ="Platform name cannot have special characters other than '+'")]
         public string PlatformName { get; set; }
 
-        [Required(ErrorMessage = "ICType cannot be blank"), MaxLength(2, ErrorMessage = "ICType cannot be more than 2 digts"), RegularExpression("^[0-9]*$", ErrorMessage = "IC Type should be numeric")]
+        [Required(ErrorMessage = "ICType cannot be blank"), MaxLength(2, ErrorMessage = "ICType cannot be more than 2 digts"), Display(Name = "IC Type"), RegularExpression("^[0-9]*$", ErrorMessage = "IC Type should be numeric")]
         public string ICType { get; set; }
 
         #endregion
-    }
+    } 
 }
